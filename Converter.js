69b718c3cd0a1a7ccd4070285a8ccfabdcd98854
kitti-katy <kitti-katy@mail.ru>
2019-01-24 13:46:01 -0500

    Added empty functions

---
+++ Converter.js
@@ -0,0 +1,60 @@
+
+  /* 
+  Supports formats: HSL, RGB, HEX, CMYK, HSV
+  Functions: 
+  1.
+  HSLtoRGB
+  HSLtoHEX
+  HSLtoCMYK
+  HSLtoHSV
+  2. 
+  RGBtoHSL
+  RGBtoHEX
+  RGBtoCMYK
+  RGBtoHSV
+  3.
+  HEXtoHSL
+  HEXtoRGB
+  HEXtoCMYK
+  HEXtoHSV
+  4.
+  CMYKtoHSL
+  CMYKtoRGB
+  CMYKtoHEX
+  CMYKtoHSV
+  5.
+  HSVtoHSL
+  HSVtoRGB
+  HSVtoHEX
+  HSVtoCMYK
+  */
+  
+  
+ HSLtoRGB = (h, s, l) => {}
+ HSLtoHEX = (h, s, l) => {}
+ HSLtoCMYK = (h, s, l) => {}
+ HSLtoHSV = (h, s, l) => {}
+
+ 
+ RGBtoHSL = (r, g, b) => {}
+ RGBtoHEX = (r, g, b) => {}
+ RGBtoCMYK = (r, g, b) => {}
+ RGBtoHSV = (r, g, b) => {}
+
+
+ HEXtoHSL = (hexString) => {}
+ HEXtoRGB = (hexString) => {}
+ HEXtoCMYK = (hexString) => {}
+ HEXtoHSV = (hexString) => {}
+
+
+ CMYKtoHSL = (c, m, y, k) => {}
+ CMYKtoRGB = (c, m, y, k) => {}
+ CMYKtoHEX = (c, m, y, k) => {}
+ CMYKtoHSV = (c, m, y, k) => {}
+
+
+ HSVtoHSL = (h, s, v) => {}
+ HSVtoRGB = (h, s, v) => {}
+ HSVtoHEX = (h, s, v) => {}
+ HSVtoCMYK = (h, s, v) => {}
